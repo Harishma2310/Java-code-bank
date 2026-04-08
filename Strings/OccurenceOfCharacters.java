766823693bce00b561797cff40f5e2f28b45047a
HARISHMA RAMESH <108524914+Harishma2310@users.noreply.github.com>
2026-04-09 01:50:26 +0530

    Create OccurenceOfCharacters.java

---
+++ Strings/OccurenceOfCharacters.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Main
+{
+    public static void main(String[] args) {
+      
+        String s = "Programming";
+        Map<Character, Integer> countMap = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            countMap.put(c, countMap.getOrDefault(c, 0) + 1);           // Update count
+        }
+
+        System.out.println("Character Occurrences:");
+        for (Map.Entry<Character, Integer> entry : countMap.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+    }
+}
